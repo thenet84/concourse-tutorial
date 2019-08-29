cf899955db72377e2d57082daf6ac21d08e8ce68
Denet <the.net.wo@gmail.com>
2019-08-29 15:25:29 +0100

    Update pipeline and task

--- tutorials/basic/my-first-example/task_my_first.sh
+++ tutorials/basic/my-first-example/task_my_first.sh
@@ -3,12 +3,3 @@
 set -e
 
 echo "hola"
-ls -R
-
-mkdir some-files
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
-
-ls some-files/*
