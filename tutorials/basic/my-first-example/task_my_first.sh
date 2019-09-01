e66fe9b15ec5a861211a17955e2270d8998b918a
Denet <the.net.wo@gmail.com>
2019-09-01 22:47:57 +0100

    Update bash script

--- tutorials/basic/my-first-example/task_my_first.sh
+++ tutorials/basic/my-first-example/task_my_first.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-echo "hola"
+echo "hello"
 
-ls -R
+ls -lR
 
 mkdir some-files
 echo "file1" > some-files/file1
@@ -13,4 +13,8 @@ echo "file3" > some-files/file3
 echo "file4" > some-files/file4
 
 ls some-files/*
+
+docker pull docker/whalesay
+
+docker/whalesay
 
